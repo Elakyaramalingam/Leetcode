81da4e2883cddc4cfda560fed6902539e445f4df
Elakyaramalingam <elakyaramalingam14@gmail.com>
2026-07-06 14:54:58 +0530

    Time: 69 ms (21.07%) | Memory: 19.5 MB (18.06%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        from collections import Counter
+        
+        need = Counter(s1)
+        window = Counter()
+        
+        for i in range(len(s2)):
+            window[s2[i]] += 1
+            
+            if i >= len(s1):
+                if window[s2[i - len(s1)]] == 1:
+                    del window[s2[i - len(s1)]]
+                else:
+                    window[s2[i - len(s1)]] -= 1
+            
+            if window == need:
+                return True
+        
+        return False
+        
